d22287ba20a49850a3f742a42fa3282ba309cc99
AdrianAlbrecht <155376@student.uwm.edu.pl>
2023-10-11 12:25:07 +0200

    lab_02_part_2

--- lab_02_QUERIES.sql
+++ lab_02_QUERIES.sql
@@ -26,10 +26,57 @@ AS wysokoœæ_dodatku
 FROM Employees
 ORDER BY Months_worked;*/
 -- QUERY 5
-SELECT Dep.Department_name
+/*SELECT Dep.Department_name, SUM(j.min_salary) AS Suma_Minimalnej, SUM(j.max_salary) AS Suma_Maxymalnej, AVG(j.min_salary) AS Œrednia_Minimalnej, AVG(j.max_salary) AS Œrednia_Maxymalnej
 FROM Departments Dep
 JOIN Job_history JH
 ON jh.department_id = dep.department_id
 JOIN JOBS J
 ON jh.job_id = j.job_id
-WHERE j.min_salary > 5000;
+WHERE j.min_salary > 5000
+GROUP BY Dep.Department_name;*/
+--Query 6
+/*SELECT e.LAST_NAME, e.department_id, dep.department_name, e.job_id 
+FROM Employees e
+JOIN Departments dep
+ON e.department_id = dep.department_id
+JOIN Locations loc
+ON dep.location_id = loc.location_id
+WHERE loc.city = 'Toronto';*/
+--Query 7
+/*SELECT e.First_name, e.Last_name
+FROM employees e
+WHERE e.department_id IN (SELECT dep.department_id 
+FROM departments dep
+JOIN employees e
+ON e.department_id = dep.department_id
+WHERE e.employee_id IN (SELECT employee_id
+FROM employees
+WHERE first_name = 'Jennifer'));*/
+--Query 8
+/*SELECT *
+FROM departments
+WHERE department_id NOT IN (SELECT UNIQUE dep.department_id
+FROM departments dep
+JOIN employees e
+ON e.department_id = dep.department_id)*/
+-- Query 9 DONE IN lab_02.sql file
+-- Query 10
+/*SELECT e.First_Name, e.Last_Name, dep.Department_name, e.salary, (SELECT jg.Grade from JOB_GRADES jg WHERE e.salary between jg.min_salary AND jg.max_salary) AS Grade
+FROM employees e
+JOIN departments dep
+ON e.department_id = dep.department_id;*/
+-- Query 11
+/*SELECT e.first_name, e.Last_name, e.salary
+FROM employees e
+WHERE e.salary > (SELECT AVG(e.salary) FROM employees e)
+ORDER BY e.salary;*/
+-- QUERY 12
+SELECT e.employee_id ,e.First_name, e.Last_name
+FROM employees e
+WHERE e.department_id IN (SELECT UNIQUE dep.department_id 
+FROM departments dep
+JOIN employees e
+ON e.department_id = dep.department_id
+WHERE e.employee_id IN (SELECT employee_id
+FROM employees
+WHERE last_name LIKE '%u%'));
